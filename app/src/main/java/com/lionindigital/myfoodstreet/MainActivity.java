c1fc58a260813f353758091cd6760a487ace781b
shyamalpunekar <shyamalsaxena@Shyamals-MacBook-Pro.local>
2020-04-15 16:47:49 -0700

    implement pop up message

--- app/src/main/java/com/lionindigital/myfoodstreet/MainActivity.java
+++ app/src/main/java/com/lionindigital/myfoodstreet/MainActivity.java
@@ -3,12 +3,24 @@ package com.lionindigital.myfoodstreet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindMyFoodStreet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindMyFoodStreet = (Button)findViewById(R.id.inputButton);
+            mFindMyFoodStreet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Hi!!!!!", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
