aa4d9a9f2d207ef349205ea5877c815450629a17
lionindigitalllc <lionindigitalllc@gmail.com>
2020-04-16 21:43:39 -0700

    login and register ui

--- app/src/main/java/com/lionindigital/myfoodstreet/ui/MainActivity.java
+++ app/src/main/java/com/lionindigital/myfoodstreet/ui/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/OstrichSans-Black.otf");
-        mAppNameTextView.setTypeface(ostrichFont);
+//        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/OstrichSans-Black.otf");
+//        mAppNameTextView.setTypeface(ostrichFont);
 
 
         mFindMyFoodStreet.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +48,51 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
+
+
     }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_main, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+    //logout
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_logout) {
+//            logout();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+
+    //logout functionality
+//    private void logout() {
+////        FirebaseAuth.getInstance().signOut();
+//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(intent);
+//        finish();
+//    }
+
+    // Click button action
+//    @Override
+//    public void onClick(View v) {
+//
+//        if(v == mFindRestaurantsButton) {
+//            Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
+//            startActivity(intent);
+//        }
+//
+//        if (v == mSavedRestaurantsButton) {
+//            Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
+//            startActivity(intent);
+//        }
+//
+//    }
 }
